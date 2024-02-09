14b20444ed6cc1cbfcf1bd0f2428fb283d7a643e
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2024-02-08 20:59:20 -0500

    Add coding exercise

---
+++ photoGallery.js
@@ -0,0 +1,15 @@
+// 1. Declare a variable name 'imageContainer' and store the html element with the id 'imageContainer'
+//  - Using querySelector, store the selector #imageContainer
+
+
+
+// 2. Add at least four image urls to the imageUrls array
+let imageUrls = [];
+
+// 3. Selecting the imageUrls array, create a forEach loop.
+
+
+// In side the body of the loop:
+//  - Create an image element and store it in a variable named 'image'
+//  - Update the src of the image element to equal the function parameter (the array element)
+//  - Append the image to the image container.
